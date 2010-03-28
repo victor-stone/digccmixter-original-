0519ab699c517d1b658860f07669dfbf44a83959
fourstones.net <fourstones.net@ab5aa816-2a3a-11df-a773-47f8c8c26a78>
2010-03-28 18:49:27 +0000

    get text for home page and footer from content manager

--- pages/home.php
+++ pages/home.php
@@ -17,9 +17,27 @@
 * $Id$
 *
 */
+require_once('lib/util.php');
+require_once('lib/dig_util.php');
+require_once('lib/query.php');
 
-	$page_title = 'dig.ccmixter &ldquo;You already have permission&hellip;&rdquo;';
-	$home_class = 'class="current"';
+// -------- SETUP QUERY -----------
+
+$topicArgs = array(
+    'dataview' => 'digtopics',
+    'datasource' => 'topics',
+    'type' => 'dig-home',
+    'ord' => 'ASC'
+);
+
+$homeQuery = new digQuery(DIG_PAGING_OFF);
+$homeQuery->ProcessAdminArgs($topicArgs);
+$homeQuery->Query();
+$topics =& $homeQuery->results;
+
+
+$page_title = 'dig.ccmixter &ldquo;You already have permission&hellip;&rdquo;';
+$home_class = 'class="current"';
 	
 	require_once('lib/head.php');
 ?>
@@ -44,31 +62,20 @@
 		<div class="page full home-description">
 			<div class="block widest first">
 				<p><strong>dig.ccmixter</strong> is divided into several sections to get you going:</p>
-				<div class="block first">
-					<h3>Podsafe Music</h3>
-				    <p>Great vocalists meet fresh, innovative producers every day on ccMixter. Together
-				    they make radio-ready tracks and remixes&hellip; <a href="/podcast_music">Search for podsafe music&hellip;</a></p>
-				</div>
-				<div class="block">
-					<h3>Instrumental Music for Film, YouTube&trade; Videos and Soundtracks</h3>
-				    <p>Instrumental background or theme music freely available&hellip; <a href="/music_for_film_and_video">Search for instrumental music for videos&hellip;</a></p>
-				</div>
-				<div class="block">
-					<h3>Music for iPods, Dance Party, Cubicle, Drive-time Enjoyment</h3>
-				    <p>Fresh hot playlists, pre-made podcasts, editors' picks are hand curated by staff and DJs, ready for your enjoyment&hellip; <a href="/featured">Download or stream our curators' picks&hellip;</a></p>
-				</div>
-				<div class="clearer"></div>
-				<div class="block first">
-					<h3>Free Music for Commercial Projects</h3>
-				    <p>If you need free music for a commercial project, we can accommodate&hellip; <a href="/free_music">Find music that is free for commercial use</a></p>
-				</div>
-				<div class="block wider">
-					<h3>For Musicians</h3>
-				    <p>All of the music on dig.ccmixter was created especially for this site. We don't take your back-catalogue
-				    or original music you already have on your hard disk (!) We want to know what new things you can do with our
-				    2,000 original a cappellas and 10,000 samples (that are all licensed under Creative Commons). If you're
-				    up for a new way of thinking about creative collaboration then come join us at <a href="http://ccmixter.org">ccMixter.org</a>.</p>
-				</div>
+            <?
+                $count = count($topics);
+                for( $i = 0; $i < $count; $i++ )
+                {
+                    $first = $i == 0 || $i == 3 ? ' first' : ($i == 4 ? ' wider' : '');
+                    $T =& $topics[$i];
+                    print "<div class=\"block{$first}\">\n" .
+                            "<h3>{$T['topic_name']}</h3>\n" .
+                            "<p>{$T['topic_text']}</p>\n" .
+                            "</div>\n";
+                    if( $i == 2 )
+                        print '<div class="clearer"></div>' . "\n";
+                }
+            ?>
 				<div class="clearer"></div>
 			</div>
 			<div class="block badge-container round">
