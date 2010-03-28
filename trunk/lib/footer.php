72dbf2021d70355e171b1f3b4ebb88f41fac89b8
fourstones.net <fourstones.net@ab5aa816-2a3a-11df-a773-47f8c8c26a78>
2010-03-28 18:49:27 +0000

    get text for home page and footer from content manager

--- trunk/lib/footer.php
+++ trunk/lib/footer.php
@@ -20,64 +20,43 @@
 ?>
 <div class="clearer"></div>
 <div id="footer">
-			<div class="footer-column first-footer-column">
-				<h4>Find music for&hellip;</h4>
-				<ul>
-					<li><a href="/podcast_music">&hellip;podcasts</a></li>
-					<li><a href="/music_for_film_and_video">&hellip;videos</a></li>
-					<li><a href="/music_for_games">&hellip;games</a></li>
-					<li><a href="/coffeeshop_music">&hellip;coffee shop</a></li>
-					<li><a href="/party_music">&hellip;party</a></li>
-					<li><a href="/cubicle_music">&hellip;cubicle</a></li>
-				</ul>
-			</div>
-			<div class="footer-column">
-				<h4>About Us</h4>
-				<ul>
-					<li><a href="/about">dig.ccMixter</a></li>
-					<li><a href="http://ccmixter.org/about">ccMixter</a></li>
-					<li><a href="http://tunetrack.net">TuneTrack</a></li>
-					<li><a href="http://tunetrack.net/artistech/pages/philosophy/">ArtIsTech Media</a></li>
-				</ul>
-			</div>
-			<div class="footer-column">
-				<h4>News</h4>
-				<ul>
-					<li><a href="http://ccmixterblog.blogspot.com">ccMixter Blog</a></li>
-					<li><a href="http://tunetrack.net/artistech/blog/">ArtIsTech Media Blog</a></li>
-					<li><a href="http://twitter.com/ccmixtermusic">Twitter</a></li>
-					<li><a href="http://ccmixter.org/view/media/extras/news">Friends of ccMixter</a></li>
-				</ul>
-			</div>
-			<div class="footer-column">
-				<h4>Information</h4>
-				<ul>
-					<li><a href="/about#credits">Site Credits</a></li>
-					<li><a href="http://ccmixter.org/privacy">Privacy Policy</a></li>
-					<li><a href="http://ccmixter.org/terms">Terms of Use</a></li>
-				</ul>
-			</div>
-			<div class="footer-column">
-				<h4>Need help?</h4>
-				<ul>
-					<li><a href="/about#help">Support</a></li>
-				</ul>
-			</div>
-			<div class="footer-column last-footer-column">
-				<h4>Designed by</h4>
-				<ul>
-					<li><a href="http://nvzion.com">nvzion</a></li>
-				</ul>
-			</div>
-			<div class="clearer"></div>
-			<p class="center site-license"><a href="http://creativecommons.org/licenses/by-nc/3.0/us/"><img src="images/by-nc.png" alt="Creative Commons Attribution-Noncommercial 3.0 United States License"></a> This text and images on this site are licensed under a <a href="http://creativecommons.org/licenses/by-nc/3.0/us/">Creative Commons Attribution-Noncommercial 3.0 United States License</a> by <a href="http://artistechmedia.com">ArtIsTech Media</a>.</p>
-		</div>
+<?
+$footerArgs = array(
+    'dataview' => 'digtopics',
+    'datasource' => 'topics',
+    'type' => 'dig-footer',
+    'ord' => 'ASC'
+);
+
+$footerQuery = new digQuery(DIG_PAGING_OFF);
+$footerQuery->ProcessAdminArgs($footerArgs);
+$footerQuery->Query();
+$topics =& $footerQuery->results;
+$count = count($topics);
+for( $i = 0; $i < $count; $i++ )
+{
+    $T =& $topics[$i];
+    $pos = $i == 0 ? ' first-footer-column' : ($i == $count-1 ? ' last-footer-column' : '');
+    print   "<div class=\"footer-column{$pos}\">\n" .
+            " <h4>{$T['topic_name']}</h4>\n" .
+            "  <ul>\n";
+    $links = preg_split('#</a>#',$T['topic_text'],-1,PREG_SPLIT_NO_EMPTY);
+    foreach( $links as $A )
+    {
+        $A = trim($A) . '</a>';
+        print "   <li>{$A}</li>\n";
+    }
+    print "  </ul>\n</div>\n";
+		
+}
+?>
+    <div class="clearer"></div>
+    <p class="center site-license"><a href="http://creativecommons.org/licenses/by-nc/3.0/us/"><img src="images/by-nc.png" alt="Creative Commons Attribution-Noncommercial 3.0 United States License"></a> This text and images on this site are licensed under a <a href="http://creativecommons.org/licenses/by-nc/3.0/us/">Creative Commons Attribution-Noncommercial 3.0 United States License</a> by <a href="http://artistechmedia.com">ArtIsTech Media</a>.</p>
+</div>
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
 </script>
 <script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-2878955-3");
-pageTracker._trackPageview();
-} catch(err) {}</script>
+try { var pageTracker = _gat._getTracker("UA-2878955-3"); pageTracker._trackPageview();} catch(err) {}
+</script>
